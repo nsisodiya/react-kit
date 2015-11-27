cd304ba3238bfe34e7150ec2e82588f8ec632b3d
Narendra Sisodiya <narendra@narendrasisodiya.com>
2015-11-27 11:53:13 +0530

    problem with react-css-module, fixed !!

--- webpack.config.js
+++ webpack.config.js
@@ -32,9 +32,10 @@ module.exports = {
 		loaders: [
 			{
 				test: /.jsx?$/,
+				exclude: /node_modules/,
 				loader: "babel",
 				query: {
-					presets: ['es2015', 'react', 'stage-0']
+					presets: ['react', 'es2015', 'stage-0']
 				}
 			},
 			{
